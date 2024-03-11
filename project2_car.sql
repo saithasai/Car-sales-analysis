14009baa26e6200adc952313fac79a066f9afbf0
SAITHA M S <84937491+saithasai@users.noreply.github.com>
2024-03-11 22:56:08 +0530

    Update project2_car.sql

--- project2_car.sql
+++ project2_car.sql
@@ -47,3 +47,55 @@ where year between 2015 and 2023;
 select * from car_proj
 where year between 2015 and 2023
 
+#9)Top selling car name by price
+select name,sum(selling_price) from car_proj
+group by name
+order by sum(selling_price) desc
+limit 1;
+
+#10)Top selling car name by count 
+select name,count(*) as num from car_proj
+group by name
+order by num desc
+limit 1;
+
+#11)Top 10 sales by year
+select year,sum(selling_price) as amount from car_proj
+group by year
+order by amount desc
+limit 10;
+
+#12)If customers prefer for joint family trip
+select name,seats,year,sum(selling_price) as amount from car_proj
+group by name,seats,year
+having seats=9
+order by seats desc;
+
+#13)If customers prefer for 2 family trip in 2019 
+select name from car_proj
+where seats>7 and year=2019
+order by seats desc
+
+#14)Fuel efficiency impact
+select name,mileage,sum(selling_price) as amount from car_proj
+group by name,mileage
+order by amount desc
+limit 10;
+
+#15)Total Number of Vehicles having different fuel Type category and their Average Price.
+select fuel,concat(round(count(fuel)*100/(select count(*) from car_proj),2),"%") as vehicle_percent,round(avg(selling_price)/100000,0) as amount_in_lakhs from car_proj
+group by fuel
+
+#16)Total Number of Vehicles having different Seat number category and their Average selling Price.
+select seats,concat(round(count(seats)*100/(select count(*) from car_proj),2),"%") as seat_percent,round(avg(selling_price)/100000,0) as amount_in_lakhs from car_proj
+group by seats;
+
+#17)Total Number of Vehicles having different engine category and their Average selling Price.
+select engine,concat(round(count(engine)*100/(select count(*) from car_proj),2),"%") as vehicle_percent,round(avg(selling_price)/100000,0) as amount_in_lakhs from car_proj
+group by engine
+order by engine;
+
+#18)Total Number of Vehicles having different max power category and their Average selling Price.
+select max_power,concat(round(count(max_power)*100/(select count(*) from car_proj),2),"%") as vehicle_percent,round(avg(selling_price)/100000,0) as amount_in_lakhs from car_proj
+group by max_power
+order by max_power;
